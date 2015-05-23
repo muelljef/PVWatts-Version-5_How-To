30a82faf805a1a0406a3e796ab512c520823f0cf
muelljef <muelljef@onid.oregonstate.edu>
2015-05-23 12:28:00 +1000

    initial setup

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jeff
+ * Date: 5/23/2015
+ * Time: 12:01 PM
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>How To</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+</body>
+</html>
